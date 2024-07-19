971a3a044bca5450ad1838b1ea7c29c8c0a51287
beroux <42benoit.roux@gmail.com>
2024-07-19 09:11:25 +0200

    first push

---
+++ main.py
@@ -0,0 +1,181 @@
+#!/bin/python3.10
+import glob
+import os
+import sys
+
+import requests
+import argparse
+import tarfile
+import json
+import shutil
+
+parser = argparse.ArgumentParser(description='Jetbrains toolbox cli')
+
+parser.add_argument("action")
+parser.add_argument("ide", nargs='?', default='all')
+parser.add_argument("version", nargs='?', default="latest")
+parser.add_argument("cmd", nargs='?')
+
+
+def open_config(path):
+    file = open(path, "r")
+    conf = json.load(file)
+    return conf
+
+
+config_fttb = open_config("/home/beroux/.config/fttb/config.json")
+args = parser.parse_args()
+
+
+def get_code(name, config_fttp: dict):
+    print(config_fttp['aliases'])
+    if name in config_fttp.values():
+        return name
+    if name in config_fttp['aliases'].keys():
+        return config_fttp['aliases'][name]
+    return name
+
+
+def download_file(url, dst):
+    with open(dst, "wb") as f:
+        response = requests.get(url, stream=True)
+        total_length = response.headers.get('content-length')
+
+        if total_length is None:
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(100 * dl / total_length)
+                print(f"{done}%", end="\r")
+                sys.stdout.flush()
+            print("")
+
+
+def list_all(ide):
+    if ide == "all":
+        res = requests.get(
+            "https://data.services.jetbrains.com/products?fields=name,intellijProductCode,description,categories")
+
+        if not res.ok:
+            print("request failed")
+            sys.exit()
+
+        ides = res.json()
+
+        for ide in ides:
+            if ide['intellijProductCode'] is not None and ide['categories'] is not None:
+                if "IDE" in ide['categories']:
+                    print(f"{ide['name']}\ncode: {ide['intellijProductCode']}\n{ide['description']}\n")
+    else:
+        print(f"print details about {ide}")
+        res = requests.get(
+            f"https://data.services.jetbrains.com/products/releases?code={ide}")
+
+        if not res.ok:
+            print("request failed")
+            sys.exit()
+
+        releases = res.json()[ide][::-1]
+
+        for release in releases:
+            print(release.keys())
+            print(release['date'])
+            if release['notesLink'] is not None:
+                print('\x1b]8;;' + release['notesLink'] + '\x1b\\' + release['version'] + '\x1b]8;;\x1b\\')
+            else:
+                print(release['version'])
+            print("")
+
+
+def search(query):
+    res = requests.get(
+        "https://data.services.jetbrains.com/products?fields=name,intellijProductCode,description,categories")
+
+    if not res.ok:
+        print("request failed")
+        sys.exit()
+
+    ides = res.json()
+
+    print(f"search for {query}")
+
+    for ide in ides:
+        if ide['intellijProductCode'] is not None and ide['categories'] is not None:
+            if "IDE" in ide['categories']:
+                if query in ide['name'] or query in ide['intellijProductCode'] or query in ide['description']:
+                    print(f"{ide['name']}\ncode: {ide['intellijProductCode']}\n{ide['description']}\n")
+
+
+def infos(ide):
+    print(f"print details about {ide}")
+    ide = get_code(ide, config_fttb)
+    res = requests.get(
+        f"https://data.services.jetbrains.com/products?fields=name,intellijProductCode,description,categories&code={ide}")
+
+    if not res.ok:
+        print("request failed")
+        sys.exit()
+
+    if len(res.json()) == 0:
+        return
+    ide = res.json()[0]
+
+    print(f"{ide['name']}\ncode: {ide['intellijProductCode']}\n{ide['description']}\n")
+
+
+def download(ide, version, config_fttp: dict):
+    if ide == "all":
+        print("invalid IDE code")
+        return
+    ide = get_code(ide, config_fttb)
+    res = requests.get(
+        f"https://data.services.jetbrains.com/products/releases?code={ide}")
+    if not res.ok:
+        print("request failed")
+        sys.exit()
+
+    releases = res.json()[ide]
+    if version == "latest":
+        version = releases[0]['version']
+
+    for release in releases:
+        if release['version'] == version:
+            print(release['downloads']['linux'])
+            filename = release['downloads']['linux']['link'].split("/")[-1]
+            filepath = f"/home/beroux/.cache/fttb/{filename}"
+            download_file(release['downloads']['linux']['link'], filepath)
+            file = tarfile.open(filepath)
+            file.extractall(path="/home/beroux/goinfre/ides/fttb/")
+            dst = file.getmembers()[0].name.split('/')[0]
+            shutil.rmtree(f"/home/beroux/goinfre/ides/fttb/{ide}-{version}")
+            os.rename(f"/home/beroux/goinfre/ides/fttb/{dst}", f"/home/beroux/goinfre/ides/fttb/{ide}-{version}")
+            return version
+
+
+def use(ide, version):
+    version = download(ide, version, config_fttb)
+    print(version)
+    try:
+        os.remove(f"/home/beroux/bin/{ide}")
+    except FileNotFoundError:
+        pass
+    ide_code = get_code(ide, config_fttb)
+    os.symlink(f"/home/beroux/goinfre/ides/fttb/{ide_code}-{version}/bin/{ide}.sh", f"/home/beroux/bin/{ide}")
+
+
+if args.action == "list":
+    list_all(args.ide)
+elif args.action == "search":
+    search(args.ide)
+elif args.action == "infos":
+    infos(args.ide)
+elif args.action == "download":
+    download(args.ide, args.version, config_fttb)
+elif args.action == "use":
+    use(args.ide, args.version)
+elif args.action.startswith("fttb://"):
+    print(args.action)
